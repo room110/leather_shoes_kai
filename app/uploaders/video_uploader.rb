2a8ed87ddbea91e3cd865803482d01e955dfd41a
vagrant <vagrant@localhost.localdomain>
2020-08-16 12:08:05 +0000

    [Finish]

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -2,8 +2,6 @@ class VideoUploader < CarrierWave::Uploader::Base
   # encoding: utf-8
 require 'streamio-ffmpeg'
 
-class VideoUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -37,24 +35,24 @@ class VideoUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
-  version :screenshot do
-    process :screenshot
-    def full_filename (for_file = model.logo.file)
-      "screenshot.jpg"
-    end
-  end
+  # version :screenshot do
+  #   process :screenshot
+  #   def full_filename (for_file = model.logo.file)
+  #     "screenshot.jpg"
+  #   end
+  # end
 
-  def screenshot
-    tmpfile = File.join(File.dirname(current_path), "tmpfile")
+  # def screenshot
+  #   tmpfile = File.join(File.dirname(current_path), "tmpfile")
 
-    File.rename(current_path, tmpfile)
+  #   File.rename(current_path, tmpfile)
 
-    movie = FFMPEG::Movie.new(tmpfile)
-    movie.screenshot(current_path + ".jpg", {resolution: '512x312' }, preserve_aspect_ratio: :width)
-    File.rename(current_path + ".jpg", current_path)
+  #   movie = FFMPEG::Movie.new(tmpfile)
+  #   movie.screenshot(current_path + ".jpg", {resolution: '512x312' }, preserve_aspect_ratio: :width)
+  #   File.rename(current_path + ".jpg", current_path)
 
-    File.delete(tmpfile)
-  end
+  #   File.delete(tmpfile)
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
@@ -68,5 +66,4 @@ class VideoUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-end
 end
